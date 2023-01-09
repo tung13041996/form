2d32a029a6310b73b9df8141f318acc3aab1a19c
Tung Nguyen <nguyenthanhtungbk96@gmail.com>
2023-01-09 17:08:58 +0700

    add style select

---
+++ vue-form/src/components/BaseSelect.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="select field">
+    <label v-if="label">{{ label }}</label>
+    <select :value="modelValue" class="field-input" v-bind="$attrs">
+      <option
+        v-for="option in categories"
+        :value="option"
+        :key="option"
+        :selected="option === modelValue"
+      >
+        {{ option }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BaseSelect",
+  props: {
+    label: {
+      type: String,
+      default: "",
+    },
+    modelValue: {
+      type: [String, Number],
+      default: "",
+    },
+    categories: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
--- vue-form/src/views/SimpleForm.vue
+++ vue-form/src/views/SimpleForm.vue
@@ -3,6 +3,11 @@
     <h1 class="txt_center">Create Form</h1>
     <div class="form-wrapper">
       <h2 class="margin-bottom-20px">Create Events</h2>
+      <BaseSelect
+        label="Category"
+        v-model="event.category"
+        :categories="event.categories"
+      />
       <BaseInput label="Title" v-model="event.title" type="text" />
       <BaseInput label="Description" v-model="event.description" type="text" />
       <h3 class="margin-bottom-20px">Where is it?</h3>
@@ -13,30 +18,64 @@
 
 <script>
 import BaseInput from "@/components/BaseInput.vue";
+import BaseSelect from "@/components/BaseSelect.vue";
 export default {
   name: "SimpleForm",
   components: {
     // eslint-disable-next-line vue/no-unused-components
     BaseInput,
+    BaseSelect,
   },
   data() {
     return {
       event: {
         title: "Title",
+        category: "Category",
         description: "Description",
         location: "Vietnam",
+        categories: ["Alex", "Peter", "Thomas", "Frank", "Steven"],
       },
     };
   },
 };
 </script>
 
-<style scoped>
+<style>
 .form-wrapper {
   max-width: 400px;
   margin: 0 auto 40px;
   padding: 40px;
   border-radius: 10px;
   border: 1px solid rgba(112, 112, 112, 0.2);
+}
+.field:not(:last-child) {
+  margin-bottom: 40px;
+}
+.field-input,
+label {
+  display: block;
+}
+label {
+  font-weight: 500;
+  margin-bottom: 5px;
+}
+.field-input {
+  height: 50px;
+  width: 100%;
+  border-radius: 5px;
+  padding: 5px 10px;
+  border: 1px solid rgba(112, 112, 112, 0.1);
+  transition: all 0.5s ease;
+  font-size: 16px;
+}
+.field-input::placeholder {
+  color: rgba(112, 112, 112, 0.5);
+  transition: all 0.5s ease;
+}
+.field-input:hover {
+  border-color: darkgreen;
+}
+.field-input:hover::placeholder {
+  color: rgba(112, 112, 112, 0.2);
 }
 </style>
